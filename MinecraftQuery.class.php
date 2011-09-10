b3814ff6fee67ccf554856bb54cdccacfc31dcf3
xPaw <thexpaw@gmail.com>
2011-09-10 13:13:10 +0300

    Properly decode received string. (Requires 'iconv')

--- MinecraftQuery.class.php
+++ MinecraftQuery.class.php
@@ -1,6 +1,4 @@
 <?php
-	// TODO: Cleanup
-	
 	function QueryMinecraft( $IP, $Port = 25565 )
 	{
 		if( !( $Resource = @FSockOpen( 'tcp://' . GetHostByName( $IP ), (int)$Port ) ) )
@@ -20,13 +18,17 @@
 			throw new Exception( "Server answered something else." );
 		}
 		
-		$Status = Socket_Get_Status( $Resource );
+		$Data = FRead( $Resource, 2 );
 		
-		if( $Status[ 'unread_bytes' ] )
+		if( StrLen( $Data ) != 2 )
 		{
-			$Data .= FRead( $Resource, $Status[ 'unread_bytes' ] );
+			FClose( $Resource );
+			throw new Exception( "WTF?" );
 		}
 		
+		$Data = UnPack( "n", $Data );
+		
+		$Data = FRead( $Resource, $Data[ 1 ] * 2 );
 		FClose( $Resource );
 		
 		if( !$Data )
@@ -34,12 +36,13 @@
 			throw new Exception( "Server did not answer anything." );
 		}
 		
+		$Data = IconV( 'UTF-16BE', 'UTF-8', $Data );
 		$Data = Explode( "\xA7", $Data );
 		
 		return Array(
-			'HostName'   => SubStr( $Data[ 0 ], 1 ),
-			'Players'    => (int)Str_Replace( "\x00", "", $Data[ 1 ] ), // Stupid notch-code fixes
-			'MaxPlayers' => (int)Str_Replace( "\x00", "", $Data[ 2 ] )
+			'HostName'   => Trim( SubStr( $Data[ 0 ], 0, -1 ) ),
+			'Players'    => IntVal( isset( $Data[ 1 ] ) ? $Data[ 1 ] : 0 ),
+			'MaxPlayers' => IntVal( isset( $Data[ 2 ] ) ? $Data[ 2 ] : 0 )
 		);
 	}
 	
