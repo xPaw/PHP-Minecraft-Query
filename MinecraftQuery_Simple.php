42a6d271c80a4df6211af956ac6616b858477cc2
Nick Smith <admin@mrplows-server.us>
2012-12-16 10:21:57 -0500

    Update MinecraftQuery_Simple.php
    Fixed error in how socket_create is called. 
    Fixed error if the sockets function is not installed properly.

--- MinecraftQuery_Simple.php
+++ MinecraftQuery_Simple.php
@@ -11,8 +11,12 @@
 	
 	function QueryMinecraft( $IP, $Port = 25565, $Timeout = 2 )
 	{
-		$Socket = Socket_Create( AF_INET, SOCK_STREAM, SOL_TCP );
-		
+		if (!extension_loaded('sockets')) {
+			return "The sockets extension is not loaded. uncomment or add 'extension=php_sockets.dll' in your .ini file";
+		}
+		else{
+		$Socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
 		Socket_Set_Option( $Socket, SOL_SOCKET, SO_SNDTIMEO, array( 'sec' => (int)$Timeout, 'usec' => 0 ) );
 		
 		if( $Socket === FALSE || @Socket_Connect( $Socket, $IP, (int)$Port ) === FALSE )
@@ -55,4 +59,5 @@
 			'Protocol'   => 0,
 			'Version'    => '1.3'
 		);
+		}
 	}
