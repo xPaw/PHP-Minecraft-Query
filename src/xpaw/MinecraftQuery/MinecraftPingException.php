0b7b503a009825fcc39e3e0e826f3f10d0ec22af
rjp2525 <renophilibert@gmail.com>
2014-02-08 18:00:02 -0500

    Fix namespace capitalization
    Rename source folder

--- src/xpaw/MinecraftQuery/MinecraftPingException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace xPaw\MinecraftQuery;
-
-// Exception thrown by MinecraftPing class
-class MinecraftPingException extends \Exception
-{
-}
--- src/xpaw/MinecraftQuery/MinecraftQueryException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace xPaw\MinecraftQuery;
-
-// Exception thrown by MinecraftQuery class
-class MinecraftQueryException extends \Exception
-{
-}
