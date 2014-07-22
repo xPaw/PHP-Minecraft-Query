ff5c4682b7078668841a4891557848d835294e72
Martin Simon <me@martinsimon.me>
2014-07-22 15:00:11 +0200

    fix standard port

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <?php
 	// Edit this ->
 	define( 'MQ_SERVER_ADDR', 'localhost' );
-	define( 'MQ_SERVER_PORT', 25566 );
+	define( 'MQ_SERVER_PORT', 25565 );
 	define( 'MQ_TIMEOUT', 1 );
 	// Edit this <-
 	
