d3d04e0ba93ab82c7261b87676f55afe743600a5
Pavel Djundik <xPaw@users.noreply.github.com>
2020-11-04 19:28:06 +0200

    Add extra bedrock fields, rename unknowns
    Fallback to null, as not all servers include all fields
    
    Fixes #154.

--- src/MinecraftQuery.php
+++ src/MinecraftQuery.php
@@ -238,16 +238,19 @@ class MinecraftQuery
 
 		$this->Info =
 		[
-			'GameName'   => $Data[ 0 ],
-			'HostName'   => $Data[ 1 ],
-			'Protocol'   => $Data[ 2 ],
-			'Version'    => $Data[ 3 ],
-			'Players'    => $Data[ 4 ],
-			'MaxPlayers' => $Data[ 5 ],
-			'Unknown2'   => $Data[ 6 ], // TODO: What is this?
-			'Map'        => $Data[ 7 ],
-			'GameMode'   => $Data[ 8 ],
-			'Unknown3'   => $Data[ 9 ], // TODO: What is this?
+			'GameName'   => $Data[ 0 ] ?? null,
+			'HostName'   => $Data[ 1 ] ?? null,
+			'Protocol'   => $Data[ 2 ] ?? null,
+			'Version'    => $Data[ 3 ] ?? null,
+			'Players'    => $Data[ 4 ] ?? null,
+			'MaxPlayers' => $Data[ 5 ] ?? null,
+			'ServerId'   => $Data[ 6 ] ?? null,
+			'Map'        => $Data[ 7 ] ?? null,
+			'GameMode'   => $Data[ 8 ] ?? null,
+			'NintendoLimited' => $Data[ 9 ] ?? null,
+			'IPv4Port'   => $Data[ 10 ] ?? null,
+			'IPv6Port'   => $Data[ 11 ] ?? null,
+			'Extra'      => $Data[ 12 ] ?? null, // What is this?
 		];
 		$this->Players = null;
 	}
